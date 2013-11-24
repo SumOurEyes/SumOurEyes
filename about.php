debe494a3b55a5fb192e3ae1a446314e345c4cdf
Alex Pojman <pojman.alex@gmail.com>
2013-11-23 19:08:11 -0600

    Add about.php

--- about.php
+++ about.php
@@ -4,8 +4,14 @@ include("header.php") ?>
 
 <html>
 <body>
-<div class="container content">
+<div class="container content left">
 		<h2>About Page</h2>
-		
+		<p>SumOurEyes is summarizing in an intelligent and collaborative way. Let us briefly explain how it works.</p>
+		<p>Go to you projects and add a new project. Within a project, add as many chapters as you want. Click on a chapter to open it.</p>
+		<p>Within a chapter, you can add notes. Type something in the textbox and click on add to add the note. Notes can be edited by just clicking on its text.</p>
+			<p>Now here comes the cool stuff: when you put [brackets] around a word,
+			it becomes a keyword for that note. Next time you mention that word in your project, it will be linked to the original note. Cool!</p>
+		<p>Want to share a project with your friends? Just go to your projects page, put your mouse on the project you want to share and click 'share'!</p>
+</div>		
 			</body>
 </html>
